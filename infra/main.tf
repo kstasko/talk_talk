b867a590561a43f92ca049cd9edea3402eeeb26a
kstasko <kstasko@users.noreply.github.com>
2023-09-04 14:57:28 -0500

    15: assume_role_with_web_identity

--- infra/main.tf
+++ infra/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  assume_role {
+  assume_role_with_web_identity {
     role_arn = "arn:aws:iam::467222377375:role/terraform-readonly"
   }
   region = "us-east-2"
@@ -20,6 +20,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "talk-talk-s3-bucket" {
   bucket = "talk-talk-${data.aws_caller_identity.current.account_id}"
+
 
   tags = {
     Name        = "talk-talk-${data.aws_caller_identity.current.account_id}"
