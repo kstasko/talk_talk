131ad03c325fd0471e32faea32369ac7bf46a094
kstasko <kstasko@users.noreply.github.com>
2023-09-04 11:17:01 -0500

    init - bucket with account id

--- infra/main.tf
+++ infra/main.tf
@@ -13,11 +13,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "first_bucket" {
-  bucket = "talk-talk-bucket"
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "talk-talk-accountName" {
+  bucket = "talk-talk-${data.aws_caller_identity.current.account_id}"
 
   tags = {
-    Name        = "talk-talk-bucket"
+    Name        = "talk-talk-${data.aws_caller_identity.current.account_id}"
     Environment = "Dev"
   }
 }
