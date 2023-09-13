a32a978a72896683b8e145d6b6c41ff68bbb42aa
kstasko <kstasko@users.noreply.github.com>
2023-09-12 21:04:06 -0500

    24: validate

--- infra/modules/create_domain/main.tf
+++ infra/modules/create_domain/main.tf
@@ -17,7 +17,7 @@ resource "aws_route53_zone" "stage-hosted-zone" {
 }
 
 resource "aws_route53_record" "stage-ns-record" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "${var.domain_name}.example.com"
   type    = "NS"
   ttl     = "30"
