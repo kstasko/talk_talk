dea877904ddd51835718c1e80a1d1474ab3ea69b
kstasko <kstasko@users.noreply.github.com>
2023-09-19 22:38:17 -0500

    logical id

--- infra/modules/create_s3_bucket/main.tf
+++ infra/modules/create_s3_bucket/main.tf
@@ -8,26 +8,39 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-resource "aws_s3_bucket" "talk_talk_s3_bucket" {
+resource "aws_s3_bucket" "this" {
   bucket = "${var.deploy_environment}.talktalk.dev"
 }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
-  bucket = aws_s3_bucket.talk_talk_s3_bucket.id
+  bucket = aws_s3_bucket.this.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  depends_on = [aws_s3_bucket_ownership_controls.this]
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
 
-  bucket = aws_s3_bucket.talk_talk_s3_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this,
+  ]
+
+  bucket = aws_s3_bucket.this.id
   acl    = "public-read"
 }
 
+
 resource "aws_s3_bucket_website_configuration" "this" {
-  bucket = aws_s3_bucket.talk_talk_s3_bucket.id
+  bucket = aws_s3_bucket.this.id
 
   index_document {
     suffix = "index.html"
@@ -40,7 +53,7 @@ resource "aws_s3_bucket_website_configuration" "this" {
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.talk_talk_s3_bucket.id
+  bucket = aws_s3_bucket.this.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -51,12 +64,12 @@ resource "aws_s3_bucket_policy" "this" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.talk_talk_s3_bucket.arn}/**"
+        Resource  = "${aws_s3_bucket.this.arn}/**"
       }
     ]
   })
 }
 
 output "s3_bucket_id" {
-  value = aws_s3_bucket.talk_talk_s3_bucket.id
+  value = aws_s3_bucket.this.id
 }
