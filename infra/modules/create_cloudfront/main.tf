b8519dab40545854eadc57bf5013659ec6c1f9d1
kstasko <kstasko@users.noreply.github.com>
2023-09-21 22:38:44 -0500

    ssl

--- infra/modules/create_cloudfront/main.tf
+++ infra/modules/create_cloudfront/main.tf
@@ -61,6 +61,7 @@ resource "aws_cloudfront_distribution" "this" {
   viewer_certificate {
     cloudfront_default_certificate = true
     acm_certificate_arn            = var.acm_certificate_arn
+    ssl_support_method             = "sni-only"
   }
 
   price_class = "PriceClass_200"
